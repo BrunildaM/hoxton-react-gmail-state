5f159edbea3c3912797029708c16bedd9f946f17
Brunilda Muco <brunildamuco6@gmail.com>
2022-07-26 23:59:14 +0200

    inbox and starred emails

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   // Use initialEmails for state
   const [emails, setEmails] = useState(initialEmails)
   const [hideRead, setHideRead] = useState(false)
+  const [currentTab, setCurrentTab] = useState(false)
 
   function toggleRead(selectedEmail) {
     const updatedEmails = JSON.parse(JSON.stringify(emails))
@@ -18,7 +19,7 @@ function App() {
   }
 
 
-  function toggleStar(selectedEmail) { 
+  function toggleStar(selectedEmail) {
     const updatedEmails = JSON.parse(JSON.stringify(emails))
     const match = updatedEmails.find(email => email.id === selectedEmail.id)
     match.starred = !match.starred
@@ -27,16 +28,43 @@ function App() {
   }
 
 
-  function toggleHideRead () {
-    setHideRead (!hideRead)
+  let emailsToShow = emails
+
+  function toggleHideRead() {
+    setHideRead(!hideRead)
+    
   }
 
-  let emailsToShow = emails
 
   if (hideRead) {
     emailsToShow = emailsToShow.filter(email => !email.read)
   }
 
+
+  function toggleStarredEmails () {
+    setCurrentTab(!currentTab)
+  }
+
+
+  if (currentTab) {
+    emailsToShow = emailsToShow.filter(email => email.starred )
+  }
+
+
+  let starredEmails = 0
+  initialEmails.forEach(email => {
+    if (email.starred === true) {
+      starredEmails += 1
+    }
+  })
+
+
+  let unreadEmails = 0
+  initialEmails.forEach(email => {
+    if (email.read === false) {
+      unreadEmails += 1
+    }
+  })
 
 
   return (
@@ -46,17 +74,21 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-          // onClick={() => {}}
+          onClick={() => {
+           toggleHideRead()
+          }}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{unreadEmails}</span>
           </li>
           <li
             className="item"
-          // onClick={() => {}}
+         onClick={() => {
+          toggleStarredEmails ()
+         }}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starredEmails}</span>
           </li>
 
           <li className="item toggle">
@@ -65,9 +97,9 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={hideRead}
-            onChange={() => {
-              toggleHideRead()
-            }}
+              onChange={() => {
+                toggleHideRead()
+              }}
             />
           </li>
         </ul>
@@ -76,19 +108,19 @@ function App() {
         <ul>
           {
             emailsToShow.map(email => (
-              <li className={`email ${email.read?  'read' : 'unread'}`}>
+              <li className={`email ${email.read ? 'read' : 'unread'}`}>
                 <input type="checkbox"
                   checked={email.read}
                   onClick={() => {
                     toggleRead(email)
                   }}
                 />
-                <input type="checkbox" 
-                className="star-checkbox" 
-                checked={email.starred} 
-                onClick={() => {
-                  toggleStar(email)
-                }}
+                <input type="checkbox"
+                  className="star-checkbox"
+                  checked={email.starred}
+                  onClick={() => {
+                    toggleStar(email)
+                  }}
                 />
                 <span>{email.sender}</span>
                 <span className="title">{email.title}</span>
